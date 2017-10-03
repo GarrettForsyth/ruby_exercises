be8d26f8f8bddd00f34026b23ff5c515464c9264
Garrett Forsyth <gtforsyth91@gmail.com>
2017-10-03 15:06:06 -0400

    enumeration methods and tests

--- enumerable_methods/enumerable_methods_rspec.rb
+++ enumerable_methods/enumerable_methods_rspec.rb
@@ -5,25 +5,7 @@ describe "my enumerable methods" do
   let(:my_array) { [10,0,-5] }
   let(:my_hash){ {one: 1, two: 2,three: 3} } 
 
-  shared_examples_for "an enumerable method" do
-
-    context "call with non-enumerable object" do
-      it { expect { nil.my_each{} }.to  raise_error (NoMethodError)}
-      it { expect { 1.my_each{} }.to raise_error(NoMethodError)}
-      it { expect { "not enum".my_each{} }.to raise_error(NoMethodError)}
-    end
-                                                                          
-    context "passed no block" do
-      it "returns the arguement that invoked it" do
-        expect(my_array.my_each).to eql(my_array)
-        expect(my_hash.my_each).to eql(my_hash)
-      end
-    end
-  end
-
   describe ".my_each"  do
-
-    it_behaves_like "an enumerable method"
 
     context "call wtih my_array" do
       it "returns the object that invoked it" do
@@ -42,6 +24,158 @@ describe "my enumerable methods" do
         expect{|x| my_hash.my_each(&x)}.to yield_successive_args([:one,1],[:two,2],[:three,3])
       end
     end
+    context "call with non-enumerable object" do
+      it { expect { nil.my_each{} }.to  raise_error (NoMethodError)}
+      it { expect { 1.my_each{} }.to raise_error(NoMethodError)}
+      it { expect { "not enum".my_each{} }.to raise_error(NoMethodError)}
+    end
+                                                                          
+    context "passed no block" do
+      it "returns the arguement that invoked it" do
+        expect(my_array.my_each).to eql(my_array)
+        expect(my_hash.my_each).to eql(my_hash)
+      end
+    end
   end
 
+  describe ".my_each_with_index"  do
+
+    context "call wtih my_array" do
+      it "returns the object that invoked it" do
+        expect(my_array.my_each_with_index{}).to eql(my_array)
+      end
+      it "yields to blcok for each element" do
+        expect{ |x| my_array.my_each_with_index(&x) }.to yield_successive_args(10,0,0,1,-5,2) 
+      end
+    end
+    context "call wtih my_hash" do
+      it "returns the object that invoked it" do
+        expect(my_hash.my_each_with_index{}).to eql(my_hash)
+      end
+      it "yields to block for each element and its index" do
+        expect{|x| my_hash.my_each_with_index(&x)}.to yield_successive_args([:one,1],0,[:two,2],1,[:three,3],2)
+      end
+    end
+    context "call with non-enumerable object" do
+      it { expect { nil.my_each_with_index{} }.to  raise_error (NoMethodError)}
+      it { expect { 1.my_each_with_index{} }.to raise_error(NoMethodError)}
+      it { expect { "not enum".my_each_with_index{} }.to raise_error(NoMethodError)}
+    end
+                                                                          
+    context "passed no block" do
+      it "returns the arguement that invoked it" do
+        expect(my_array.my_each_with_index).to eql(my_array)
+        expect(my_hash.my_each_with_index).to eql(my_hash)
+      end
+    end
+  end
+
+  describe ".my_select"  do
+
+    context "call wtih my_array" do
+      it "returns new object with only elements that met criteria" do
+        expect(my_array.my_select{|x| x >= 0}).to eql([10,0])
+      end
+      it "yields to block for each element" do
+        expect{ |x| my_array.my_select(&x) }.to yield_successive_args(10,0,-5) 
+      end
+    end
+
+    context "call wtih my_hash one param" do
+
+      it "returns new object with only elements that met criteria" do
+        expect(my_hash.my_select{|x| x == :one}).to eql({one: 1})
+      end
+    end
+
+    context "call with my_hash with two params" do
+
+      it "returns new object with only elements that met criteria" do
+        expect(my_hash.my_select{|x,y| y == 2}).to eql({two: 2})
+      end
+    end
+
+
+    context "call with non-enumerable object" do
+      it { expect { nil.my_select{} }.to  raise_error (NoMethodError)}
+      it { expect { 1.my_select{} }.to raise_error(NoMethodError)}
+      it { expect { "not enum".my_select{} }.to raise_error(NoMethodError)}
+    end
+
+    context "passed no block" do
+      it "returns empty" do
+        expect(my_array.my_select).to eql([])
+        expect(my_hash.my_select).to eql({})
+      end
+    end
+  end
+
+  describe ".my_all?" do
+
+    context "call wtih my_array" do
+      it "returns true when all elements satisify condition" do
+        expect(my_array.my_all?{|x| x > -100}).to eql(true)
+      end
+      it "returns false when at least one element does not satisify" do
+        expect(my_array.my_all?{|x| x > 0}).to eql(false)
+      end
+    end
+
+    context "call with non-enumerable object" do
+      it { expect { nil.my_all?{} }.to  raise_error (NoMethodError)}
+      it { expect { 1.my_all?{} }.to raise_error(NoMethodError)}
+      it { expect { "not enum".my_all?{} }.to raise_error(NoMethodError)} end
+                                                                          
+    context "passed no block" do
+      it "returns true" do
+        expect(my_array.my_all?).to eql(true)
+      end
+    end
+  end
+
+  describe ".my_any?" do
+                                                                              
+    context "call wtih my_array" do
+      it "returns true when any element satifies the condition" do
+        expect(my_array.my_any?{|x| x > 0}).to eql(true)
+      end
+      it "returns false when zero elements  satisify" do
+        expect(my_array.my_any?{|x| x > 1000}).to eql(false)
+      end
+    end
+                                                                              
+    context "call with non-enumerable object" do
+      it { expect { nil.my_any?{} }.to  raise_error (NoMethodError)}
+      it { expect { 1.my_any?{} }.to raise_error(NoMethodError)}
+      it { expect { "not enum".my_any?{} }.to raise_error(NoMethodError)} end
+                                                                          
+    context "passed no block" do
+      it "returns true" do
+        expect(my_array.my_any?).to eql(true)
+      end
+    end
+  end
+
+  describe ".my_none?" do
+
+    context "call wtih my_array" do
+      it "returns true when no elements satifies the condition" do
+        expect(my_array.my_none?{|x| x < -1000}).to eql(true)
+      end
+      it "returns false when any elements  satisify" do
+        expect(my_array.my_none?{|x| x > 1}).to eql(false)
+      end
+    end
+                                                                              
+    context "call with non-enumerable object" do
+      it { expect { nil.my_none?{} }.to  raise_error (NoMethodError)}
+      it { expect { 1.my_none?{} }.to raise_error(NoMethodError)}
+      it { expect { "not enum".my_none?{} }.to raise_error(NoMethodError)} end
+                                                                          
+    context "passed no block" do
+      it "returns true" do
+        expect(my_array.my_none?).to eql(true)
+      end
+    end
+  end
 end
--- enumerable_methods/lib/enumerable_methods.rb
+++ enumerable_methods/lib/enumerable_methods.rb
@@ -14,4 +14,140 @@ module  Enumerable
     self
   end
 
+  # Returns all elements of an array or hash and their indices
+  # Calls a block for each element, and then its respective index
+  def my_each_with_index
+    errorMessage = "#each_with_index  must be called on an Enumerable Object"
+    raise(NoMethodError, errorMessage) unless self.is_a? Enumerable 
+    return self unless block_given?
+    return self unless self.any?   
+    index = 0;
+    # use .entries here to hashes are formatted right, e.g. they
+    # return as [key, value]
+    items = self.entries
+    for i in 0...items.length do
+      yield(items[i])
+      yield(i)
+    end
+    self
+  end
+
+  # Returns an array/hash containing all elements of enum for which which
+  # the given block returns a true value.
+  def my_select(&block)
+    errorMessage = "#my_select must be called on an Enumerable Object"
+    raise(NoMethodError, errorMessage) unless self.is_a? Enumerable 
+    return self unless self.any?   
+
+    if self.instance_of? Array
+      selection = []
+      return selection unless block_given?
+      self.my_each {|i| selection << i if yield(i)}
+      return selection
+
+    elsif self.instance_of? Hash
+      selection = {}
+      return selection unless block_given?
+
+      # If only block param is passed, check for matching keys 
+      if block.arity==1 
+        i = 0
+        self.my_each do |entry|
+           selection[entry[0]]=entry[1] if yield(entry[0]) && i%2==0
+           i+=1
+        end
+        return selection
+      # If two block paramters are passed, check for matching keys
+      # and values
+      elsif block.arity==2
+        self.my_each do |key,value|
+           selection[key]=value if yield(key,value) 
+        end                                                    
+        return selection
+      else
+        raise(IllegalArgumentError, "Too many arguments to block")
+      end
+    else
+      raise(IllegalArgumentError, "#my_select must be called on an Array or Hash.") 
+    end
+  end
+
+
+  # Passes all elements to a block and returns true if they all
+  # satisfiy a condition
+  def my_all?
+    errorMessage = "#my_all? must be called on an Enumerable Object"
+    raise(NoMethodError, errorMessage) unless self.is_a? Enumerable      
+    return true unless block_given?
+    return true unless self.any?   
+    my_each{|x| return false unless yield(x)}
+    return true
+  end
+
+  # Passes all elements to a block and returns true if any of them
+  # satisfiy the condition
+  def my_any?
+
+    errorMessage = "#my_any? must be called on an Enumerable Object"    
+    raise(NoMethodError, errorMessage) unless self.is_a? Enumerable      
+    return true unless block_given?
+    return true unless self.any?   
+    my_each{|x| return true if yield(x)}
+    return false
+  end
+
+  # Passes each elemetn to a block and returns true if none of them
+  # satisfy the block's condition
+  def  my_none? 
+    errorMessage = "#my_none? must be called on an Enumerable Object"
+    raise(NoMethodError, errorMessage) unless self.is_a? Enumerable      
+    return true unless block_given?
+    return true unless self.any?   
+    my_each{|x| return false if yield(x)}
+    return true
+  end
+
+  # Returns the number of items in enum through enumeration.
+  # If an argument is given, counts how many times this argument
+  # occurs
+  # if a block is given, returns how many times block is true
+  def my_count arg=nil, &block
+    errorMessage = "#my_none? must be called on an Enumerable Object"     
+    raise(NoMethodError, errorMessage) unless self.is_a? Enumerable      
+    return self.length if arg.nil? && !block_given?
+    my_count_no_var(&block) if arg.nil?
+
+    if block_given?
+      counted_items = my_select(&block)
+      return counted_items.length
+    else
+      counted_items = my_select{|x| x == arg}
+      return counted_items.length
+    end
+  end
+
+  def my_count_no_var &block
+    if block_given?
+      counted_items = my_select(&block)
+      return counted_items.length
+    else
+      return self.length
+    end
+  end
+  
+  # returns a new array with the results of running the block once 
+  # for every element in the enum
+  def my_map(&block)
+    mapped_array = []
+    my_each{|x| mapped_array << &block.call(x)}
+    mapped_array
+  end
+
+  # Combines all elements of enum byapplying a binary opeartion specified
+  # by a block. alieas for 'inject"
+  def my_reduce acc=self[0], &block
+    my_each { |x| acc = block.call(acc, x)}
+    acc
+  end
 end
+
