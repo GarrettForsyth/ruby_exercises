2abda045b1441fc5b9bbebef90101843addf8e49
Garrett Forsyth <gtforsyth91@gmail.com>
2017-10-09 15:54:11 -0400

    add knight rules

--- chess/lib/chess_rules.rb
+++ chess/lib/chess_rules.rb
@@ -1,33 +1,35 @@
 require "~/the_odin_project/ruby_exercises/chess/lib/move.rb"
 # This class defines methods that model the rules of chess
+# TODO: REFACTOR IF CONDITIONS INTO SEPARATE FUNCTIONS
+# ------> Keep functions smalllllllll!
 class ChessRules
 
   ########################################################
   # A pawn move is legal if:
-  #   - it moves one square foward to un unoccupied sqaure
-  #   - it moves two squares fowards and:
+  #   - it moves one square forward to un-unoccupied square
+  #   - it moves two squares forwards and:
   #       -it's the pawns first move
   #       -neither the square it crosses or lands on 
   #        is occupied
-  #   - it can move one sqaure diagonally foward if:
-  #       -it is captruing a piece
+  #   - it can move one square diagonally forward if:
+  #       -it is capturing a piece
   #       -(this included enpassant)
   ########################################################
   def isValidPawnMove?(move, board)
     possibleMoves = getPossibleMoves(move, board)
-    x = move.piece.colour == :white ? 1 : -1 # pawns can only move foward
+    x = move.piece.colour == :white ? 1 : -1 # pawns can only move forward
 
     # for each of the possible moves, check
     possibleMoves.each do |move|
 
       squareInbetween = board.getSquaresInbetween(move.from, move.to)
-      # validate moving foward one
+      # validate moving forward one
       if((x*(move.to[1].to_i - move.from[1].to_i) == 1) && # one row difference
          (move.to[0] == move.from[0]) &&                   # same column
          (not board.getSquare(move.to).occupied?) )        # destination unoccupied
         return true
 
-      ## validates movoing foward 2 if first move
+      ## validates moving forward 2 if first move
       elsif((x*(move.to[1].to_i - move.from[1].to_i) == 2) &&    # two row difference     
            (move.to[0] == move.from[0]) &&                       # same column
            (not board.getSquare(move.to).occupied?) &&           # destination unoccupied
@@ -64,6 +66,31 @@ class ChessRules
     end
   end
 
+  def isValidKnighMove?(move, board)
+    possibleMoves = getPossibleMoves(move, board)
+
+    possibleMoves.each do |move|
+
+              # Enforce L Shape
+      if( (((move.to[1].to_i - move.from[1].to_i).abs == 2 &&
+              (move.to[0].ord - move.from[0].ord).abs == 1) ||
+        
+          ((move.to[0].ord - move.from[0].ord).abs == 2 &&
+              (move.to[1].to_i - move.from[1].to_i).abs == 1)) &&
+        
+
+          # can only land on empty squares or capture
+          (board.getSquare(move.to).occupied? == false || 
+          (board.getSquare(move.to).occupied? == true &&
+          board.getSquare(move.to).occupancy.colour != move.piece.colour)))
+        return true
+
+      else
+        return false
+      end
+    end
+  end
+
   # Returns true if there is a piece blocking the move
   def impeded?(move, board)
     coords = board.getSquaresInbetween(move.from,move.to)
@@ -84,13 +111,18 @@ class ChessRules
         possibleMoves << possibleMove
       end
     else
-      possibleMoves << move
+      # Make sure the piece trying to be moved exists
+      if(board.getSquare(move.from).occupied? &&
+         board.getSquare(move.from).occupancy.class == move.piece.class &&
+         board.getSquare(move.from).occupancy.colour == move.piece.colour)
+         possibleMoves << move
+      end
     end
     possibleMoves
   end
 
   # Tries to infer where piece is coming from 
-  # Returns an array of coordiantes of squares that contain move.piece
+  # Returns an array of coordinates of squares that contain move.piece
   # on board
   def getPossibleFrom move,board
     possibleFrom = []
--- chess/spec/chess_rspec.rb
+++ chess/spec/chess_rspec.rb
@@ -457,11 +457,73 @@ describe ChessRules do
       it "returns false when taking own piece vertically" do
         board.setSquare("d1", Rook.new(:black))              
         board.setSquare("d8", Rook.new(:black))
-        m1 = Move.new(board.getPieceAt("d1"), "d8", "a1")
+        m1 = Move.new(board.getPieceAt("d1"), "d8", "d1")
         m2 = Move.new(board.getPieceAt("d8"), "d1", "d8")
                                                              
         expect(rules.isValidRookMove?(m1, board)).to eq false
         expect(rules.isValidRookMove?(m2, board)).to eq false
+      end
+    end
+
+    describe "#isValidKnightMove?" do
+      it "returns true when moving in L" do
+        board.setSquare("e4", Knight.new(:black))              
+        m1 = Move.new(board.getPieceAt("e4"), "c3","e4")
+        m2 = Move.new(board.getPieceAt("e4"), "g3","e4")
+        m3 = Move.new(board.getPieceAt("e4"), "d6","e4")
+        m4 = Move.new(board.getPieceAt("e4"), "f6","e4")
+        m5 = Move.new(board.getPieceAt("e4"), "g5","e4")
+        m6 = Move.new(board.getPieceAt("e4"), "c5","e4")
+        m7 = Move.new(board.getPieceAt("e4"), "d2","e4")
+        m8 = Move.new(board.getPieceAt("e4"), "f2","e4")
+
+
+        expect(rules.isValidKnighMove?(m1, board)).to eq true
+        expect(rules.isValidKnighMove?(m2, board)).to eq true
+        expect(rules.isValidKnighMove?(m3, board)).to eq true
+        expect(rules.isValidKnighMove?(m4, board)).to eq true
+        expect(rules.isValidKnighMove?(m5, board)).to eq true
+        expect(rules.isValidKnighMove?(m6, board)).to eq true
+        expect(rules.isValidKnighMove?(m7, board)).to eq true
+        expect(rules.isValidKnighMove?(m8, board)).to eq true
+      end
+
+      it "returns false when moving in row" do
+        board.setSquare("e4", Knight.new(:black))              
+        m1 = Move.new(board.getPieceAt("e4"), "e5","e4")
+        m2 = Move.new(board.getPieceAt("e4"), "e3","e4")
+        expect(rules.isValidKnighMove?(m1, board)).to eq false
+        expect(rules.isValidKnighMove?(m2, board)).to eq false
+      end
+      
+      it "returns false when moving in col" do
+        board.setSquare("e4", Knight.new(:black))              
+        m1 = Move.new(board.getPieceAt("e4"), "a4","e4")
+        m2 = Move.new(board.getPieceAt("e4"), "h4","e4")
+        expect(rules.isValidKnighMove?(m1, board)).to eq false
+        expect(rules.isValidKnighMove?(m2, board)).to eq false
+      end
+
+      it "returns false when moving in diagonal" do
+        board.setSquare("e4", Knight.new(:black))              
+        m1 = Move.new(board.getPieceAt("e4"), "f3","e4")
+        m2 = Move.new(board.getPieceAt("e4"), "d5","e4")
+        expect(rules.isValidKnighMove?(m1, board)).to eq false
+        expect(rules.isValidKnighMove?(m2, board)).to eq false
+      end
+
+      it "returns false when moving into its own piece" do
+        board.setSquare("e4", Knight.new(:black))              
+        board.setSquare("c3", Knight.new(:black))              
+        m1 = Move.new(board.getPieceAt("e4"), "c3","e4")
+        expect(rules.isValidKnighMove?(m1, board)).to eq false
+      end
+
+      it "returns true when capturing"  do
+        board.setSquare("e4", Knight.new(:black))              
+        board.setSquare("c3", Knight.new(:white))              
+        m1 = Move.new(board.getPieceAt("e4"), "c3","e4")
+        expect(rules.isValidKnighMove?(m1, board)).to eq true
       end
     end
   end
