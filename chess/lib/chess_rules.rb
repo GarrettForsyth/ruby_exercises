f705e69a5058a890f4a6890e9bc299e6a9176cf3
Garrett Forsyth <gtforsyth91@gmail.com>
2017-10-10 17:43:31 -0400

    refactored chess_rules.rb

--- chess/lib/chess_rules.rb
+++ chess/lib/chess_rules.rb
@@ -1,151 +1,130 @@
 require "~/the_odin_project/ruby_exercises/chess/lib/move.rb"
 # This class defines methods that model the rules of chess
-# TODO: REFACTOR IF CONDITIONS INTO SEPARATE FUNCTIONS
-# ------> Keep functions smalllllllll!
+
 class ChessRules
 
-  ########################################################
-  # A pawn move is legal if:
-  #   - it moves one square forward to un-unoccupied square
-  #   - it moves two squares forwards and:
-  #       -it's the pawns first move
-  #       -neither the square it crosses or lands on 
-  #        is occupied
-  #   - it can move one square diagonally forward if:
-  #       -it is capturing a piece
-  #       -(this included enpassant)
-  ########################################################
   def isValidPawnMove?(move, board)
     possibleMoves = getPossibleMoves(move, board)
-    x = move.piece.colour == :white ? 1 : -1 # pawns can only move forward
+    dir = move.piece.colour == :white ? 1 : -1 # pawns can only move forward
 
-    # for each of the possible moves, check
     possibleMoves.each do |move|
-
-      squareInbetween = board.getSquaresInbetween(move.from, move.to)
-      # validate moving forward one
-      if((x*(move.to[1].to_i - move.from[1].to_i) == 1) && # one row difference
-         (move.to[0] == move.from[0]) &&                   # same column
-         (not board.getSquare(move.to).occupied?) )        # destination unoccupied
-        return true
-
-      ## validates moving forward 2 if first move
-      elsif((x*(move.to[1].to_i - move.from[1].to_i) == 2) &&    # two row difference     
-           (move.to[0] == move.from[0]) &&                       # same column
-           (not board.getSquare(move.to).occupied?) &&           # destination unoccupied
-           (not board.getSquare(squareInbetween[0]).occupied?)&& # not blocked
-           (move.piece.firstMove))                               # pawn's first move
-          return true
-
-      ### validates pos captures
-      elsif( ((move.to[0].ord - move.from[0].ord).abs == 1) &&     # one row up or down
-             (x*(move.to[1].to_i - move.from[1].to_i) == 1) && # one col difference
-             (board.getSquare(move.to).occupied?) && 
-             (board.getSquare(move.to).occupancy.colour != move.piece.colour)) # capture
-          return true 
-             
-      else 
-        return false
-      end
+      return true if isLegalOneSquarePawnMove?(board, move, dir)
+      return true if isLegalTwoSquarePawnMove?(board, move, dir)
+      return true if isLegalPawnCapture?(board,move, dir)
     end
+    return false
+  end
+
+  def isLegalOneSquarePawnMove?(board, move, dir)
+     return dir*(move.to[1].to_i - move.from[1].to_i) == 1 &&  # one row difference     
+            move.to[0] == move.from[0] &&                      # same column
+            isLandingOnEmptySquare?(board,move)                # destination unoccupied
+  end
+
+  def isLegalTwoSquarePawnMove?(board, move, dir)
+    return dir*(move.to[1].to_i - move.from[1].to_i) == 2 &&   # two row difference     
+           move.to[0] == move.from[0] &&                       # same column
+           isLandingOnEmptySquare?(board,move) &&              # destination unoccupied
+           isNotImpeded?(move,board) &&                        # not blocked
+           move.piece.firstMove                                # pawn's first move
+  end
+
+  def isLegalPawnCapture?(board, move, dir)
+    return ((move.to[0].ord - move.from[0].ord).abs == 1) &&     # one row up or down 
+           (dir*(move.to[1].to_i - move.from[1].to_i) == 1) &&   # one col difference
+           isCapture?(board,move)
   end
 
   def isValidRookMove?(move, board)
     possibleMoves = getPossibleMoves(move,board)
-
     possibleMoves.each do |move|
-      if( (move.to[0] == move.from[0] || move.to[1] == move.from[1]) && ## same row/column
-          (not impeded?(move, board)) && 
-          (board.getSquare(move.to).occupied? == false || 
-          (board.getSquare(move.to).occupied? == true &&
-          board.getSquare(move.to).occupancy.colour != move.piece.colour)))
-        return true
-      else
-        return false
-      end
+      return true if isMoveHorizontalOrVerticalLine?(board,move) &&
+                     isNotImpeded?(move, board) && 
+                     isCaptureOrLandingOnEmptySquare?(board,move)
     end
+    return false
+  end
+
+  def isMoveHorizontalOrVerticalLine?(board,move)
+    return move.to[0] == move.from[0] || move.to[1] == move.from[1]
   end
 
   def isValidKnightMove?(move, board)
     possibleMoves = getPossibleMoves(move, board)
-
     possibleMoves.each do |move|
-
-              # Enforce L Shape
-      if( (((move.to[1].to_i - move.from[1].to_i).abs == 2 &&
-              (move.to[0].ord - move.from[0].ord).abs == 1) ||
-        
-          ((move.to[0].ord - move.from[0].ord).abs == 2 &&
-              (move.to[1].to_i - move.from[1].to_i).abs == 1)) &&
-        
-
-          # can only land on empty squares or capture
-          (board.getSquare(move.to).occupied? == false || 
-          (board.getSquare(move.to).occupied? == true &&
-          board.getSquare(move.to).occupancy.colour != move.piece.colour)))
-        return true
-
-      else
-        return false
-      end
+      return true if isMoveLShaped?(board,move) && 
+                     isCaptureOrLandingOnEmptySquare?(board,move)
     end
+    return false
+  end
+
+  def isMoveLShaped?(board,move)
+    return ((move.to[1].to_i - move.from[1].to_i).abs == 2 &&
+            (move.to[0].ord - move.from[0].ord).abs == 1) ||
+                                                            
+           ((move.to[0].ord - move.from[0].ord).abs == 2 &&
+            (move.to[1].to_i - move.from[1].to_i).abs == 1) 
   end
 
   def isValidBishopMove?(move, board)
     possibleMoves = getPossibleMoves(move, board)
-
     possibleMoves.each do |move|
-
-          # Enforce diagonal i.e. rise == run 
-      if( ((move.to[0].ord - move.from[0].ord).abs == 
-            (move.to[1].to_i - move.from[1].to_i).abs) &&
-
-           (not impeded?(move, board)) && 
-           # can only land on empty squares or capture
-           (board.getSquare(move.to).occupied? == false || 
-           (board.getSquare(move.to).occupied? == true &&
-           board.getSquare(move.to).occupancy.colour != move.piece.colour)))
-        return true
-      else
-        return false
-      end
+      return true if isMoveAlongDiagonal?(board,move) &&
+                     isNotImpeded?(move, board) && 
+                     isCaptureOrLandingOnEmptySquare?(board,move)
     end
+    return false
   end
+
+  def isMoveAlongDiagonal?(board, move)
+    return (move.to[0].ord - move.from[0].ord).abs == 
+           (move.to[1].to_i - move.from[1].to_i).abs
+  end
+
 
   def isValidQueenMove?(move, board)
     possibleMoves = getPossibleMoves(move, board)
     possibleMoves.each do |move|
-      if (isValidRookMove || isValidBishopMove)
-        return true
-      else
-        return false
-      end
+      return true if (isValidRookMove || isValidBishopMove)
     end
+    return false
   end
 
   def isValidKingMove?(move, board)
     possibleMoves = getPossibleMoves(move, board) 
     possibleMoves.each do |move|
-      if( ((move.to[0].ord - move.from[0].ord).between?(-1,1))   &&
-          ((move.to[1].to_i - move.from[1].to_i).between?(-1,1)) &&
-          # can only land on empty squares or capture
-          (board.getSquare(move.to).occupied? == false || 
-          (board.getSquare(move.to).occupied? == true &&
-          board.getSquare(move.to).occupancy.colour != move.piece.colour)))
-        return true
-      else
-        return false
-      end
-    end
-  end
-
-  # Returns true if there is a piece blocking the move
-  def impeded?(move, board)
-    coords = board.getSquaresInbetween(move.from,move.to)
-    coords.each do |coord|
-      return true if board.getSquare(coord).occupied?
+      return true if isMoveOneSquareAway?(board, move) &&
+                     isCaptureOrLandingOnEmptySquare?(board,move) 
     end
     return false
+  end
+
+  def isMoveOneSquareAway?(board, move)
+    return (move.to[0].ord - move.from[0].ord).between?(-1,1)   &&
+           (move.to[1].to_i - move.from[1].to_i).between?(-1,1) 
+  end
+
+  def isCaptureOrLandingOnEmptySquare?(board, move)
+      return isLandingOnEmptySquare?(board, move) || isCapture?(board,move)  
+
+  end
+
+  def isCapture?(board,move)
+    return board.getSquare(move.to).occupied? == true &&
+           board.getSquare(move.to).occupancy.colour != move.piece.colour
+  end
+
+  def isLandingOnEmptySquare?(board, move)
+      return board.getSquare(move.to).occupied? == false  
+  end
+
+  # Returns true if there are no pieces blocking the move
+  def isNotImpeded?(move, board)
+    coords = board.getSquaresInbetween(move.from,move.to)
+    coords.each do |coord|
+      return false if board.getSquare(coord).occupied?
+    end
+    return true
   end
 
 
